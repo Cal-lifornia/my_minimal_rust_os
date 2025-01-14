d034bb18b150d74ba8afd72bee93ee1ce514ee6f
Cal-lifornia <76858909+Cal-lifornia@users.noreply.github.com>
2025-01-14 15:49:09 +1100

    Created testing suite for library and started integration tests.

--- flake.nix
+++ flake.nix
@@ -1,86 +1,59 @@
 {
-  description = "A Nix-flake-based Rust development environment";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
-
   outputs =
     {
       self,
       nixpkgs,
+      flake-utils,
       rust-overlay,
+      ...
     }:
-    let
-      supportedSystems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-      forEachSupportedSystem =
-        f:
-        nixpkgs.lib.genAttrs supportedSystems (
-          system:
-          f {
-            pkgs = import nixpkgs {
-              inherit system;
-              overlays = [
-                rust-overlay.overlays.default
-                self.overlays.default
-              ];
-            };
-          }
-        );
-    in
-    {
-      overlays.default = final: prev: {
-        rustToolchain =
-          let
-            rust = prev.rust-bin;
-          in
-          if builtins.pathExists ./rust-toolchain.toml then
-            rust.fromRustupToolchainFile ./rust-toolchain.toml
-          else if builtins.pathExists ./rust-toolchain then
-            rust.fromRustupToolchainFile ./rust-toolchain
-          else
-            rust.nightly."2024-12-20".default.override {
-              extensions = [
-                "rust-src"
-                "rustfmt"
-                "clippy"
-              ];
-            };
-      };
 
-      devShells = forEachSupportedSystem (
-        { pkgs }:
-        {
-          default = pkgs.mkShell {
-            packages = with pkgs; [
-              rustToolchain
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            buildInputs = [
               openssl
               pkg-config
+              rust-analyzer
               cargo-deny
               cargo-edit
               cargo-watch
-              rust-analyzer
               cargo-insta
               cargo-bootimage
+              (rust-bin.selectLatestNightlyWith (
+                toolchain:
+                toolchain.default.override {
+                  extensions = [
+                    "rust-src"
+                    "rustfmt"
+                    "clippy"
+                    "llvm-tools"
+                  ];
+                }
+              ))
             ];
 
             env = {
-              # Required by rust-analyzer
-              RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
+              # RUST_SRC_PATH = "{pkgs.rust-bin}/lib/rustlib/src/rust/library";
             };
+
           };
-        }
-      );
-    };
+      }
+    );
 
 }
--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -5,33 +5,22 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+
 use my_minimal_rust_os::println;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     my_minimal_rust_os::test_panic_handler(info)
 }
 
-#[cfg(test)]
 #[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
+fn test_println() {
+    println!("test_println output");
 }
